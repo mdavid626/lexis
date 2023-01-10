4bb76a0e0a108ae05e29c231f39beace91b50fbd
Dávid Molnár <mdavid626@gmail.com>
2023-01-10 14:12:10 +0100

    Drop IDE files

--- src/ifj_project_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ ifj_project - Debug ]----------
-	@"mingw32-make.exe"  -j 2 -f "ifj_project.mk"
-clean:
-	@echo ----------Cleaning project:[ ifj_project - Debug ]----------
-	@"mingw32-make.exe"  -j 2 -f "ifj_project.mk" clean
